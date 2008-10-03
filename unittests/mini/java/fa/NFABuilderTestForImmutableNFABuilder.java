7eb3a93dbfe7bc36bd62570782fc7ddaccb97dfb
lemontree.cool <lemontree.cool@8d7d388d-804b-0410-8f49-193e9cfc9f8b>
2008-10-03 08:49:30 +0000

    1. legacy unit tests removed 2. DFATest revised to use factory method for implementations 3. NFABuilderTest and NFATest merged

--- unittests/mini/java/fa/NFABuilderTestForImmutableNFABuilder.java
+++
@@ -1,8 +0,0 @@
-package mini.java.fa;
-
-
-public class NFABuilderTestForImmutableNFABuilder extends NFABuilderTest {
-    protected NFABuilder createNFABuilder() {
-        return new ImmutableNFA.Builder();
-    }
-}
--- unittests/mini/java/fa/NFATestForNFAAdapter.java
+++ unittests/mini/java/fa/NFATestForNFAAdapter.java
@@ -3,13 +3,8 @@ package mini.java.fa;
 import mini.java.fa.legacy.v1.NFAAdapter;
 
 
-public class NFATestForNFAAdapter extends NFATest {    
-    public NFATestForNFAAdapter() {
-        super();
-    }
-    
-    @Override
-    protected NFA createNFA() {
-        return TestHelper.buildNFA("ABa,BCb,CDc,AC,AD,DE", new NFAAdapter());
+public class NFATestForNFAAdapter  extends NFATest {
+    protected NFABuilder getBuilder() {
+        return new NFAAdapter();
     }
 }
