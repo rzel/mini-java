c71b0b93a935f5b797e23dc2df87fe9c2e79c19e
lemontree.cool <lemontree.cool@8d7d388d-804b-0410-8f49-193e9cfc9f8b>
2009-07-18 02:39:48 +0000

    Parser & ParserConfig merged

--- src/mini/java/syntax/Parser.java
+++ src/mini/java/syntax/Parser.java
@@ -1,20 +1,41 @@
 package mini.java.syntax;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import mini.java.fa.NFAClosure;
+import mini.java.fa.NFAState;
+import mini.java.fa.helper.Helper;
+import mini.java.fa.helper.IFinderCallback;
+import mini.java.fa.helper.NFAClosureFinder;
+import mini.java.fa.helper.NFAStateFinder;
 
 public class Parser {
     
-    private final ParserConfig _conf;
-    private final static Terminal END = new Terminal(RuleSet.END);
+    private static boolean DEBUG = false;
     
-    public Parser(ParserConfig conf_) {
-        _conf = conf_;
-    }
+    private final ParserState _engine;
+    private final RuleSet _rules;
+    private final static Terminal END = new Terminal(RuleSet.END);
+
     
     public Parser(RuleSet rules_) {
-        _conf = new ParserConfig(rules_);
+        _rules = rules_;
+        _engine = buildEngine(rules_);
+    }
+    
+    public ParserState getEngine() {
+        return _engine;
+    }
+
+
+    public RuleSet getRuleSet() {
+        return _rules; 
     }
 
     /**
@@ -30,7 +51,7 @@ public class Parser {
         List<ParserState> stack = new LinkedList<ParserState>();
         
         
-        stack.add(_conf.getEngine());
+        stack.add(_engine);
         
         while (!in.isEmpty()) { // should never be empty
             Symbol nextSymbol = in.remove(0);
@@ -83,17 +104,95 @@ public class Parser {
             }
         }
         
-//        if (out.size() != 1) {
-//            throw new RuntimeException("Parsing error: Missing elements...");
-//        }
-//        
-//        NonTerminal ret = (NonTerminal)out.get(0);
-//        if (!ret.getType().equals(RuleSet.START)) {
-//            throw new RuntimeException("Parsing error: Illegal grammar -- " + _conf.getRuleSet());
-//        }
-//        
-//        return ret;
-        
         throw new RuntimeException("Parsing error: Reaching the end of input queue.");
     }
+    
+    
+    protected static ParserState buildEngine(RuleSet rules_) {
+        if (rules_ == null || rules_.getRules().length <= 0) {
+            throw new IllegalArgumentException("Invalid rule set: null/empty");
+        }
+        
+        
+        // **Generates the NFA represented by the rule set
+        ParserState nfaRoot = ParserState.createRoot(); // this is the root for NFA
+        final Map<String, ParserState> nonTerminalStates = new HashMap<String, ParserState>();
+        
+        // create an NFA state for each of the non-terminals
+        {
+            nonTerminalStates.put(RuleSet.START, nfaRoot);
+            
+            Set<String> nonTerminals = new HashSet<String>(rules_.getNonTerminals());
+            nonTerminals.remove(RuleSet.START); // already added
+            for (String nonTerminal : nonTerminals) {
+                nonTerminalStates.put(nonTerminal, new ParserState(nfaRoot));
+            }
+        }
+        
+        
+        for (Rule rule : rules_.getRules()) {
+            // "items" are presented by NFA states
+            String lhs = rule.getLeftSymbol();
+            ParserState prev = nonTerminalStates.get(lhs);
+            
+            for (String rhs : rule.getRightSymbols()) {                
+                ParserState curr = new ParserState(nfaRoot);
+                prev.addTransition(curr, rhs);
+                // for non-terminals, add an epsilon to their corresponding nfa states
+                if (nonTerminalStates.containsKey(rhs)) {
+                    prev.addTransition(nonTerminalStates.get(rhs));
+                }
+                prev = curr;
+            }            
+            
+            prev.addRule(rule); // add the rule to the final state
+        }
+        
+        if (DEBUG) {
+            final StringBuilder sb = new StringBuilder();
+            Helper.visit(nfaRoot, new NFAStateFinder(
+                    new IFinderCallback<NFAState>() {
+                        public boolean onNext(NFAState src_, NFAState dest_, Object input_) {
+                            sb.append(String.format("%s => %s(%s)%n",
+                                    src_, dest_, input_));
+                            return true;
+                        }
+                    }));
+            System.err.print(sb);
+            System.err.println("----");
+        }
+        
+        // **Generates the actual parser engine
+        final Map<NFAClosure, NFAState> mapping = new HashMap<NFAClosure, NFAState>();
+        final ParserState engine = ParserState.createRoot();
+        NFAClosure init = nfaRoot.getClosure();
+        mapping.put(init, engine);
+        
+        Helper.visit(init, new NFAClosureFinder(
+                new IFinderCallback<NFAClosure>() {
+                    public boolean onNext(NFAClosure src_, NFAClosure dest_, Object input_) {
+                        assert(input_ != null);
+                        
+                        if (!mapping.containsKey(dest_)) { // new mapping
+                            ParserState engineState = new ParserState(engine);
+                            // copy the rules from the NFA to the engine
+                            for (NFAState state : dest_.getStates()) {
+                                ParserState st = (ParserState)state;
+                                if (st.canReduce()) {
+                                    engineState.addRules(st.getRules());
+                                }
+                            }
+                            mapping.put(dest_, engineState);
+                        }
+                        
+                        // add the corresponding transitions in the newly created DFA
+                        NFAState src = mapping.get(src_);
+                        src.addTransition(mapping.get(dest_), input_);
+                        return true;
+                    }
+                }));
+        
+        return engine;
+    }
+
 }
--- unittests/mini/java/syntax/ParserTest.java
+++ unittests/mini/java/syntax/ParserTest.java
@@ -2,9 +2,12 @@ package mini.java.syntax;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.util.LinkedList;
 import java.util.List;
+
+import mini.java.fa.helper.Helper;
 
 import org.junit.Test;
 
@@ -91,6 +94,49 @@ public class ParserTest {
             assertNotNull(ret);
             assertEquals("START(E(E(E(E((,E(C),)),*),|,E(C)),E(C)))", ret.toString());
         }
+    }
+    
+    
+    @Test(expected=RuntimeException.class)
+    public void testReduceReduceConflict() {
+        RuleSet rules = new RuleSet();
+        rules.addRule(new Rule().left(RuleSet.START).right("S"));
+        rules.addRule(new Rule().left("S").right("A"));
+        rules.addRule(new Rule().left("S").right("B"));
+        rules.addRule(new Rule().left("A").right("1"));
+        rules.addRule(new Rule().left("B").right("1"));
+        
+        Parser.buildEngine(rules);        
+        fail("This grammar cannot be handled by LR(1); ParserConfig should be able to detect this.");
+    }
+    
+    @Test
+    public void testReduceReduceNoConflict() {
+        RuleSet rules = new RuleSet();
+        Rule A = new Rule().left("A").right("1");
+        Rule B = new Rule().left("B").right("1");
+        rules.addRule(new Rule().left(RuleSet.START).right("S"));
+        rules.addRule(new Rule().left("S").right("A", "1"));
+        rules.addRule(new Rule().left("S").right("B", "2"));
+        rules.addRule(A);
+        rules.addRule(B);
+        
+
+        ParserState engine = Parser.buildEngine(rules);
+        assertEquals(
+                "0 =>(1) 1\n" + 
+                "0 =>(A) 2\n" + 
+                "0 =>(B) 3\n" +
+                "0 =>(S) 4\n" +
+                "2 =>(1) 5\n" +
+                "3 =>(2) 6\n", Helper.dump(engine));
+        
+        ParserState target = (ParserState)engine.getState("1");
+        assertNotNull(target);
+        assertNotNull(target.getRules());
+        assertEquals(2, target.getRules().length);
+        assertEquals(A, target.getRule("1"));
+        assertEquals(B, target.getRule("2"));
     }
 
 }
